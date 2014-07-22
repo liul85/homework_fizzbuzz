c17e337a932cc3b1b08091205695561d42a2946d
liul85 <liul85@gmail.com>
2014-07-22 10:33:51 +0800

    add rake task for test

---
+++ Rakefile
@@ -0,0 +1,6 @@
+desc "Run rspec test"
+task :test do
+	sh "bundle exec rspec spec/fizzbuzz_spec.rb --format doc"
+end
+
+task :default => :test
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 require 'FizzBuzz'
 
 describe "FizzBuzz" do
-  it "should puts 'FizzBuzz' if num%3 == 0 and num%5 == 0" do
+  it "should return 'FizzBuzz' if num%3 == 0 and num%5 == 0" do
     expect(FizzBuzz.new.fizzbuzz(15)).to eq('FizzBuzz')
   end
 
-  it "should puts 'Fizz' if num%3 == 0" do
+  it "should return 'Fizz' if num%3 == 0" do
     expect(FizzBuzz.new.fizzbuzz(3)).to eq('Fizz')
   end
 
@@ -14,12 +14,12 @@ describe "FizzBuzz" do
     expect(FizzBuzz.new.fizzbuzz(55)).to eq('Buzz')
   end
 
-  it "should puts 'Err' if num <= 0" do
+  it "should return 'Err' if num <= 0" do
     expect(FizzBuzz.new.fizzbuzz(0)).to eq('Err')
     expect(FizzBuzz.new.fizzbuzz(-2)).to eq('Err')
   end
 
-  it "should puts num self" do
+  it "should return num self" do
 	  expect(FizzBuzz.new.fizzbuzz(1)).to eq('1')
   end
 end
