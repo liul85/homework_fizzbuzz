a8baaa6dc6c3f3944d424b9aa487d5722aa84e48
liul85 <liul85@gmail.com>
2014-08-06 12:41:48 +0800

    update test

--- spec/splitarray_spec.rb
+++ spec/splitarray_spec.rb
@@ -9,7 +9,7 @@ describe SplitArray do
 
   it "should return correct splited array2" do
     arr = (1..11).to_a
-    expect(SplitArray.new.splitarray(arr, 3)).to eq([[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9]])
+    expect(SplitArray.new.splitarray(arr, 4)).to eq([[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8]])
   end
 
   it "should return correct splited array3" do
